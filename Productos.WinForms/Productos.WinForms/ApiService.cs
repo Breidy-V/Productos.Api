b4418baaa7d3752b7bc8a1f52e8d4c16359df36c
Breidy <vasquezbreidy3@gmail.com>
2026-09-21 21:38:36 -0400

    Implementar clientes WinForms y Blazor

---
+++ Productos.WinForms/Productos.WinForms/ApiService.cs
@@ -0,0 +1,41 @@
+﻿using System.Net.Http.Json;
+
+namespace Productos.WinForms
+{
+    public class ApiService
+    {
+        private readonly HttpClient _httpClient;
+
+        public ApiService()
+        {
+            _httpClient = new HttpClient
+            {
+                BaseAddress = new Uri("http://localhost:5069/")
+            };
+        }
+
+        public async Task<List<Producto>> ObtenerProductosAsync()
+        {
+            return await _httpClient.GetFromJsonAsync<List<Producto>>("api/Productos")
+                   ?? new List<Producto>();
+        }
+        public async Task<bool> CrearProductoAsync(Producto producto)
+        {
+            var response = await _httpClient.PostAsJsonAsync("api/Productos", producto);
+
+            return response.IsSuccessStatusCode;
+        }
+        public async Task<bool> ActualizarProductoAsync(int id, Producto producto)
+        {
+            var response = await _httpClient.PutAsJsonAsync($"api/Productos/{id}", producto);
+
+            return response.IsSuccessStatusCode;
+        }
+        public async Task<bool> EliminarProductoAsync(int id)
+        {
+            var response = await _httpClient.DeleteAsync($"api/Productos/{id}");
+
+            return response.IsSuccessStatusCode;
+        }
+    }
+}
---
+++ Productos.WinForms/Productos.WinForms/Producto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Productos.WinForms
+{
+    public class Producto
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; } = "";
+        public decimal Precio { get; set; }
+        public int Stock { get; set; }
+    }
+}
